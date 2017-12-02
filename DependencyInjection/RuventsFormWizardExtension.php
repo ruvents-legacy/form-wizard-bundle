fc160083041bee8fc2af552c85278761e880e2ca
Valentin <udaltsov.valentin@gmail.com>
2017-12-02 21:14:55 +0300

    PHP-based DI config

--- DependencyInjection/RuventsFormWizardExtension.php
+++ DependencyInjection/RuventsFormWizardExtension.php
@@ -3,16 +3,12 @@ declare(strict_types=1);
 
 namespace Ruvents\FormWizardBundle\DependencyInjection;
 
-use Ruvents\FormWizardBundle\EventListener\StepNotFoundExceptionListener;
-use Ruvents\FormWizardBundle\Storage\SessionStorage;
-use Ruvents\FormWizardBundle\Storage\StorageInterface;
 use Ruvents\FormWizardBundle\Type\StepTypeInterface;
-use Ruvents\FormWizardBundle\Type\TypeFacadeFactory;
 use Ruvents\FormWizardBundle\Type\WizardTypeInterface;
-use Ruvents\FormWizardBundle\WizardFactory;
-use Ruvents\FormWizardBundle\WizardFactoryInterface;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
 class RuventsFormWizardExtension extends Extension
 {
@@ -21,24 +17,8 @@ class RuventsFormWizardExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $container->autowire('ruvents_form_wizard.factory.default', WizardFactory::class)
-            ->setPublic(false);
-
-        $container->setAlias(WizardFactoryInterface::class, 'ruvents_form_wizard.factory.default')
-            ->setPublic(false);
-
-        $container->autowire(TypeFacadeFactory::class)
-            ->setPublic(false);
-
-        $container->autowire('ruvents_form_wizard.storage.session', SessionStorage::class)
-            ->setPublic(false);
-
-        $container->setAlias(StorageInterface::class, 'ruvents_form_wizard.storage.session')
-            ->setPublic(false);
-
-        $container->register(StepNotFoundExceptionListener::class)
-            ->setPublic(false)
-            ->addTag('kernel.event_subscriber');
+        (new PhpFileLoader($container, new FileLocator(__DIR__.'/../Resources/config')))
+            ->load('services.php');
 
         $container->registerForAutoconfiguration(WizardTypeInterface::class)
             ->addTag('ruvents_form_wizard.wizard_type');
